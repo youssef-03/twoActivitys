b166496eda2c54c6306bb6be33dd5cf117b4c963
youssef_03 <cherguiyoussef2003@gmail.com>
2022-11-15 17:44:27 +0100

    java done

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText inputsend;
+    TextView txtreply;
+    Button btnsend;
+    String messageSent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent2 = getIntent();
+        String text = intent2.getStringExtra("message2");
+        txtreply = findViewById(R.id.textReply);
+        txtreply.setText(text);
+        inputsend = findViewById(R.id.inputSend);
+        txtreply = findViewById(R.id.textReply);
+        btnsend = findViewById(R.id.btnSend);
+        btnsend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                messageSent = inputsend.getText().toString();
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtra("message",messageSent);
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -2,13 +2,38 @@ package com.example.twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    TextView txtsent;
+    Button btnreply;
+    EditText inputreply;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+
+        String text = intent.getStringExtra("message");
+        txtsent = findViewById(R.id.textSend);
+        txtsent.setText(text);
+        btnreply = findViewById(R.id.btnReply);
+        btnreply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String messagereplied;
+                inputreply = findViewById(R.id.inputReply);
+                messagereplied = inputreply.getText().toString();
+                Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+                intent.putExtra("message2",messagereplied);
+                startActivity(intent);
+            }
+        });
+
     }
 }
